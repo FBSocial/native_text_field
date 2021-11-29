645a71bce91b0acc1b4b0f2194ab4a08b3b866ed
Soto.Pu <p709723778@126.com>
2021-11-29 09:29:35 +0000

    !3 feat: 输入框优化(还没有确认输入的字不进行计算长度及其他优化) * feat: 输入框优化

--- ios/Classes/NativeTextField+TextField.swift
+++ ios/Classes/NativeTextField+TextField.swift
@@ -7,8 +7,7 @@
 
 import Foundation
 
-extension NativeTextField : UITextFieldDelegate {
-    
+extension NativeTextField: UITextFieldDelegate {
     func initTextField(frame: CGRect, args: [String: Any]?) {
         let initText = (args?["text"] as? String) ?? ""
         let textStyle = (args?["textStyle"] as? [String: Any])
@@ -20,12 +19,12 @@ extension NativeTextField : UITextFieldDelegate {
         let keyboardType = args?["keyboardType"] as? String
         let allowRegExp = (args?["allowRegExp"] as? String) ?? ""
         let readOnly = (args?["readOnly"] as? Bool) ?? false
-        
+
         defaultAttributes = textStyle2Attribute(textStyle: textStyle, defaultAttr: defaultAttributes)
         let placeHolderStyleAttr = textStyle2Attribute(textStyle: placeHolderStyle, defaultAttr: defaultAttributes)
-        
+
         textField = UITextField(frame: frame)
-        textField.attributedText = NSMutableAttributedString(string: initText,attributes: defaultAttributes)
+        textField.attributedText = NSMutableAttributedString(string: initText, attributes: defaultAttributes)
         textField.delegate = self
         textField.font = defaultAttributes[.font] as? UIFont
         textField.textColor = defaultAttributes[.foregroundColor] as? UIColor ?? UIColor.black
@@ -34,13 +33,13 @@ extension NativeTextField : UITextFieldDelegate {
         textField.keyboardType = string2KeyboardType(str: keyboardType)
         textField.attributedPlaceholder = NSAttributedString(string: placeHolder, attributes: placeHolderStyleAttr)
         textField.isUserInteractionEnabled = !readOnly
-        
+
         textField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
         self.maxLength = maxLength
         self.allowRegExp = allowRegExp
         if done { textField.returnKeyType = .done }
     }
-    
+
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         if textField.returnKeyType == .done {
             submitText()
@@ -48,8 +47,7 @@ extension NativeTextField : UITextFieldDelegate {
         }
         return true
     }
-    
-    
+
     func textFieldDidEndEditing(_ textField: UITextField) {
         updateFocus(focus: false)
     }
@@ -57,31 +55,99 @@ extension NativeTextField : UITextFieldDelegate {
     func textFieldDidBeginEditing(_ textField: UITextField) {
         updateFocus(focus: true)
     }
-    
+
     @objc func textFieldDidChange() {
+        let selectedRange = textField.markedTextRange
+        let position = textField.position(from: selectedRange?.start ?? UITextPosition(), offset: 0)
+
+        // 获取高亮部分
+        if selectedRange != nil && position != nil {
+            return
+        }
+
+        let textContent: String = textField.text!
+        let len = textContent.count
+        if len > maxLength {
+            let reqIndex = textContent.index(textContent.startIndex, offsetBy: maxLength)
+            setText(text: String(textContent[..<reqIndex]))
+        }
         updateText(text: textField.text ?? "")
     }
-    
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        textField.typingAttributes = defaultAttributes
+
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString text: String) -> Bool {
+        /// 回车 或者 完成按钮
+        if text == "\n" && textField.returnKeyType == .done {
+            submitText()
+            return false
+        }
+
+        /// 为空说明是删除
+        if text.isEmpty {
+            return true
+        }
+
+        /// 文本内容正则匹配
         if allowRegExp.count > 0 {
             let re = try? NSRegularExpression(pattern: allowRegExp, options: NSRegularExpression.Options.caseInsensitive)
-            let count = re?.matches(in: string, options: NSRegularExpression.MatchingOptions.withoutAnchoringBounds, range: NSRange(location: 0, length: string.count)).count ?? 0
-            if count != string.count { return false }
+            let count = re?.matches(in: text, options: NSRegularExpression.MatchingOptions.withoutAnchoringBounds, range: NSRange(location: 0, length: text.count)).count ?? 0
+            if count != text.count { return false }
         }
-        
-        if let text = textField.text, let _range = Range(range, in: text) {
-            let newText = text.replacingCharacters(in: _range, with: string)
-            if maxLength != 0 && newText.count > maxLength {
-                let canInputLength = maxLength - textField.text!.count + range.length
-                if canInputLength > 0 {
-                    let value = text.replacingCharacters(in: _range, with: string.prefix(canInputLength))
-                    textField.text = value
-                    updateText(text: value)
+
+        let markedTextRange = textField.markedTextRange
+        let position = textField.position(from: markedTextRange?.start ?? UITextPosition(), offset: 0)
+
+        // 获取高亮部分
+        if markedTextRange != nil && position != nil {
+            return true
+        }
+
+        textField.typingAttributes = defaultAttributes
+
+        let comcatstr = textField.text!.replacingCharacters(in: Range(range, in: textField.text!)!, with: text)
+        let canInputLen = maxLength - comcatstr.count
+
+        if canInputLen >= 0 {
+            return true
+        } else {
+            let len = text.count + canInputLen
+            // 防止当text.length + caninputlen < 0时，使得rg.length为一个非法最大正数出错
+            let rg = NSRange(location: 0, length: max(len, 0))
+
+            if rg.length > 0 {
+                var tempStr: String = ""
+                // 判断是否只普通的字符或asc码(对于中文和表情返回false)
+                let canBeASC: Bool = text.canBeConverted(to: String.Encoding.ascii)
+                if canBeASC {
+                    let textIndex = text.index(text.startIndex, offsetBy: rg.length)
+                    tempStr = String(text[..<textIndex]) // 因为是ascii码直接取就可以了不会错
+                } else {
+                    var tempStrIndex = 0
+                    var trimString = "" // 截取出的字串
+                    let range = text.startIndex ..< text.endIndex
+
+                    // 使用字符串遍历，这个方法能准确知道每个emoji是占一个unicode还是两个
+                    text.enumerateSubstrings(in: range, options: String.EnumerationOptions.byComposedCharacterSequences) { substring, _, _, stop in
+                        if tempStrIndex >= rg.length {
+                            stop = true
+                            return
+                        }
+                        if substring == nil {
+                            return
+                        }
+                        trimString = trimString.appending(substring!)
+                        tempStrIndex += 1
+                    }
+
+                    tempStr = trimString
                 }
-                return false
+                // rang是指从当前光标处进行替换处理(注意如果执行此句后面返回的是true会触发didchange事件)
+                setText(text: textField.text!.replacingCharacters(in: Range(range, in: textField.text!)!, with: tempStr))
+                DispatchQueue.main.async {
+                    textField.selectedTextRange = textField.textRange(from: textField.endOfDocument, to: textField.endOfDocument)
+                    self.updateText(text: textField.text!)
+                }
             }
+            return false
         }
-        return true
     }
 }
--- ios/Classes/NativeTextField.swift
+++ ios/Classes/NativeTextField.swift
@@ -5,25 +5,24 @@
 //  Created by lionel.hong on 2021/5/11.
 //
 
-import Foundation
 import Flutter
+import Foundation
 import UIKit
 
-class NativeTextField : NSObject,FlutterPlatformView {
-    
-    var viewId:Int64 = -1
-    var textField:UITextField!
-    var textView:PlaceholderTextView!
-    var channel:FlutterMethodChannel!
-    var maxLength:Int = 0
-    var allowRegExp = ""//" |[a-zA-Z]|[\u{4e00}-\u{9fa5}]|[0-9]"
-    
+class NativeTextField: NSObject, FlutterPlatformView {
+    var viewId: Int64 = -1
+    var textField: UITextField!
+    var textView: PlaceholderTextView!
+    var channel: FlutterMethodChannel!
+    var maxLength: Int = 0
+    var allowRegExp = "" // " |[a-zA-Z]|[\u{4e00}-\u{9fa5}]|[0-9]"
+
     var defaultAttributes: [NSAttributedString.Key: Any] = [
         .font: UIFont.systemFont(ofSize: 14, weight: UIFont.Weight.light),
         .foregroundColor: UIColor.black,
     ]
-    
-    init(frame: CGRect, viewId: Int64, args:Any?,  messenger: FlutterBinaryMessenger) {
+
+    init(frame: CGRect, viewId: Int64, args: Any?, messenger: FlutterBinaryMessenger) {
         // 处理通信
         self.viewId = viewId
         // 页面初始化
@@ -35,22 +34,21 @@ class NativeTextField : NSObject,FlutterPlatformView {
             _frame = CGRect(x: frame.origin.x, y: frame.origin.y, width: width, height: height ?? 40)
         }
         super.init()
-        
+
         channel = FlutterMethodChannel(name: "com.fanbook.native_textfield_\(viewId)", binaryMessenger: messenger)
-        channel.setMethodCallHandler { [weak self] (call, result) in
+        channel.setMethodCallHandler { [weak self] call, result in
             self?.handlerMethodCall(call, result)
         }
-        
+
         let maxLines = (args?["maxLines"] as? Int) ?? 1
         if maxLines == 1 {
             initTextField(frame: _frame, args: args)
         } else {
             initTextView(frame: _frame, args: args)
         }
-        
     }
-    
-    func handlerMethodCall(_ call: FlutterMethodCall, _ result: FlutterResult)  {
+
+    func handlerMethodCall(_ call: FlutterMethodCall, _ result: FlutterResult) {
         switch call.method {
         case "updateFocus":
             if let focus = call.arguments as? Bool {
@@ -62,7 +60,7 @@ class NativeTextField : NSObject,FlutterPlatformView {
             }
             break
         case "setText":
-            if let text = call.arguments as? String , text != textField?.text ?? textView?.text ?? "" {
+            if let text = call.arguments as? String, text != textField?.text ?? textView?.text ?? "" {
                 print("set: \(text)")
                 setText(text: text)
             }
@@ -71,17 +69,13 @@ class NativeTextField : NSObject,FlutterPlatformView {
             break
         }
     }
-    
+
     func view() -> UIView {
         return textField ?? textView
     }
-
-    
-    
 }
 
-extension NativeTextField : UITextViewDelegate {
-    
+extension NativeTextField: UITextViewDelegate {
     func initTextView(frame: CGRect, args: [String: Any]?) {
         let initText = (args?["text"] as? String) ?? ""
         let textStyle = (args?["textStyle"] as? [String: Any])
@@ -93,13 +87,12 @@ extension NativeTextField : UITextViewDelegate {
         let keyboardType = args?["keyboardType"] as? String
         let allowRegExp = (args?["allowRegExp"] as? String) ?? ""
         let readOnly = (args?["readOnly"] as? Bool) ?? false
-        
+
         defaultAttributes = textStyle2Attribute(textStyle: textStyle, defaultAttr: defaultAttributes)
         let placeHolderStyleAttr = textStyle2Attribute(textStyle: placeHolderStyle, defaultAttr: defaultAttributes)
-        
-        
+
         textView = PlaceholderTextView(frame: frame)
-        textView.attributedText = NSMutableAttributedString(string: initText,attributes: defaultAttributes)
+        textView.attributedText = NSMutableAttributedString(string: initText, attributes: defaultAttributes)
         textView.delegate = self
         textView.font = defaultAttributes[.font] as? UIFont
         textView.textAlignment = string2textAlignment(str: textAlign)
@@ -112,86 +105,145 @@ extension NativeTextField : UITextViewDelegate {
         self.allowRegExp = allowRegExp
         if done { textView.returnKeyType = .done }
     }
-    
-    
+
     func textViewDidBeginEditing(_ textView: UITextView) {
         updateFocus(focus: true)
     }
-    
+
     func textViewDidEndEditing(_ textView: UITextView) {
         updateFocus(focus: false)
     }
-    
+
     func textViewDidChange(_ textView: UITextView) {
+        let selectedRange = textView.markedTextRange
+        let position = textView.position(from: selectedRange?.start ?? UITextPosition(), offset: 0)
+
+        // 获取高亮部分
+        if selectedRange != nil && position != nil {
+            return
+        }
+
+        let textContent: String = textView.text
+        let len = textContent.count
+        if len > maxLength {
+            let reqIndex = textContent.index(textContent.startIndex, offsetBy: maxLength)
+            setText(text: String(textContent[..<reqIndex]))
+        }
         updateText(text: textView.text ?? "")
     }
-    
+
     func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
-        textView.typingAttributes = defaultAttributes
+        /// 回车 或者 完成按钮
         if text == "\n" && textView.returnKeyType == .done {
             submitText()
             return false
         }
-        if let _text = textView.text, let _range = Range(range, in: _text) {
-            let newText = _text.replacingCharacters(in: _range, with: text)
-            if maxLength != 0 && newText.count > maxLength && text.count != 0 {
-                let canInputLength = maxLength - textView.text!.count + range.length
-                if canInputLength > 0 {
-                    let value = _text.replacingCharacters(in: _range, with: text.prefix(canInputLength))
-                    textView.text = value
-                    updateText(text: value)
-                }
-                return false
-            }
-            
+
+        /// 为空说明是删除
+        if text.isEmpty {
+            return true
         }
-        return true
+
+        let markedTextRange = textView.markedTextRange
+        let position = textView.position(from: markedTextRange?.start ?? UITextPosition(), offset: 0)
+
+        // 获取高亮部分
+        if markedTextRange != nil && position != nil {
+            return true
+        }
+
+        textView.typingAttributes = defaultAttributes
+
+        let comcatstr = textView.text.replacingCharacters(in: Range(range, in: textView.text)!, with: text)
+        let canInputLen = maxLength - comcatstr.count
+
+        if canInputLen >= 0 {
+            return true
+        } else {
+            let len = text.count + canInputLen
+            // 防止当text.length + caninputlen < 0时，使得rg.length为一个非法最大正数出错
+            let rg = NSRange(location: 0, length: max(len, 0))
+
+            if rg.length > 0 {
+                var tempStr: String = ""
+                // 判断是否只普通的字符或asc码(对于中文和表情返回false)
+                let canBeASC: Bool = text.canBeConverted(to: String.Encoding.ascii)
+                if canBeASC {
+                    let textIndex = text.index(text.startIndex, offsetBy: rg.length)
+                    tempStr = String(text[..<textIndex]) // 因为是ascii码直接取就可以了不会错
+                } else {
+                    var tempStrIndex = 0
+                    var trimString = "" // 截取出的字串
+                    let range = text.startIndex ..< text.endIndex
+
+                    // 使用字符串遍历，这个方法能准确知道每个emoji是占一个unicode还是两个
+                    text.enumerateSubstrings(in: range, options: String.EnumerationOptions.byComposedCharacterSequences) { substring, _, _, stop in
+                        if tempStrIndex >= rg.length {
+                            stop = true
+                            return
+                        }
+                        if substring == nil {
+                            return
+                        }
+                        trimString = trimString.appending(substring!)
+                        tempStrIndex += 1
+                    }
+
+                    tempStr = trimString
+                }
+                // rang是指从当前光标处进行替换处理(注意如果执行此句后面返回的是true会触发didchange事件)
+                setText(text: textView.text.replacingCharacters(in: Range(range, in: textView.text)!, with: tempStr))
+                DispatchQueue.main.async {
+                    textView.selectedTextRange = textView.textRange(from: textView.endOfDocument, to: textView.endOfDocument)
+                    self.updateText(text: textView.text)
+                }
+            }
+            return false
+        }
     }
-    
-    
 }
 
 extension NativeTextField {
-    func textStyle2Attribute(textStyle :[String: Any]?, defaultAttr :[NSAttributedString.Key: Any]?) -> [NSAttributedString.Key: Any] {
+    func textStyle2Attribute(textStyle: [String: Any]?, defaultAttr: [NSAttributedString.Key: Any]?) -> [NSAttributedString.Key: Any] {
         guard let textStyle = textStyle else {
             return defaultAttr ?? [:]
         }
         let textColorValue = (textStyle["color"] as? Int) ?? 0
         let fontSize = (textStyle["fontSize"] as? Int) ?? 14
         let height = (textStyle["height"] as? Double) ?? 1.17
-        let textColor = UIColor.init(color: textColorValue)
+        let textColor = UIColor(color: textColorValue)
         let weight = (textStyle["fontWeight"] as? Int) ?? 3
-        
+
         return [
-            .font: UIFont.systemFont(ofSize: CGFloat(fontSize), weight: convertWeight(fontWeight:weight)),
+            .font: UIFont.systemFont(ofSize: CGFloat(fontSize), weight: convertWeight(fontWeight: weight)),
             .foregroundColor: textColor,
         ]
     }
-    
+
     /// Flutter 为 int  Swift 是CGFloat (并且对应的值是完全不一样的,所以要做对应关系)
-    func convertWeight( fontWeight:Int) -> UIFont.Weight {
+    func convertWeight(fontWeight: Int) -> UIFont.Weight {
         if fontWeight == 0 {
             return UIFont.Weight.ultraLight
-        }else if fontWeight == 1 {
+        } else if fontWeight == 1 {
             return UIFont.Weight.thin
-        }else if fontWeight == 2 {
+        } else if fontWeight == 2 {
             return UIFont.Weight.light
-        }else if fontWeight == 3 {
+        } else if fontWeight == 3 {
             return UIFont.Weight.regular
-        }else if fontWeight == 4 {
+        } else if fontWeight == 4 {
             return UIFont.Weight.medium
-        }else if fontWeight == 5 {
+        } else if fontWeight == 5 {
             return UIFont.Weight.semibold
-        }else if fontWeight == 6 {
+        } else if fontWeight == 6 {
             return UIFont.Weight.bold
-        }else if fontWeight == 7 {
+        } else if fontWeight == 7 {
             return UIFont.Weight.heavy
-        }else if fontWeight == 8 {
+        } else if fontWeight == 8 {
             return UIFont.Weight.black
         }
         return .regular
     }
-    
+
     func string2textAlignment(str: String?) -> NSTextAlignment {
         guard let str = str else {
             return .left
@@ -211,7 +263,7 @@ extension NativeTextField {
             return .left
         }
     }
-    
+
     func string2KeyboardType(str: String?) -> UIKeyboardType {
         guard let str = str else {
             return .default
@@ -229,5 +281,4 @@ extension NativeTextField {
             return .default
         }
     }
-    
 }
